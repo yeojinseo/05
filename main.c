bff9788a36b3b3dd57b2046b79eabfbcbcbcaa01
yeojinseo <113421483+yeojinseo@users.noreply.github.com>
2022-10-04 17:06:45 +0900

    03

--- main.c
+++ main.c
@@ -4,15 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
+	char c;
+	int num = 0;
 	
-	printf("정수 하나를 입력하시오 : ");
-	scanf("%d", &x);
-	
-	if (x < 0)
-		x = -1*x;
-	
-	printf("절대값은 %d 입니다.\n", x);
+	printf("input a string : ");
+	while ( (c = getchar()) != '\n') // 입력문자가 개행문자가 나올 때까지 반복 
+	{
+		if ( c >= '0' && c <= '9' ) // 입력된 글자(c)가 숫자인가?
+			num = num + 1; // 그렇다면 num에 하나를 센다.
+	}
+	 
+	printf("the number of digits is %d\n", num); //출력
 		
 	return 0;
 }
